0c4d6bcc6558f12acb5479d1868948f69bbb4338
Abhinav Verma <abhinavv@gwu.edu>
2018-01-26 11:49:30 -0500

    Using JPA Entities

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"domain"})
 public class Spring5webappApplication {
 
 	public static void main(String[] args) {
